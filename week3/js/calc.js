1e2b2ebffa2a33d662ae1ff852930bc0c863080f
Elena-Sea <o.smetyuk@gmail.com>
2022-10-10 11:03:25 +0200

    Adds code to task 5 and styles the legends

--- week3/js/calc.js
+++ week3/js/calc.js
@@ -72,7 +72,6 @@ const btnStory = document.querySelector('.story-btn');
 const outputStory = document.querySelector('.output-story');
 
 const displayStory = () => { 
-// outputStory.textContent = `Timmie Willie is a ${inputAdjective.value} mouse who is accidentally taken to a city in a vegetable basket. When he wakes up, he finds himself at a party and makes a friend.`;
 
 
     if (inputNoun.value === "" || inputVerb.value === "" || inputAdverb.value === "" || inputAdjective.value === "") {
@@ -80,8 +79,6 @@ const displayStory = () => {
     } else { 
         outputStory.textContent = `Timmie Willie is a ${inputAdjective.value.toUpperCase()} ${inputNoun.value.toUpperCase()} who is ${inputAdverb.value.toUpperCase()} taken to a city in a vegetable basket. When he wakes up, he finds himself at a party and ${inputVerb.value.toUpperCase()} a friend.`;
         
-
-
     }
 }
 
@@ -89,3 +86,45 @@ btnStory.addEventListener("click", displayStory);
 
 
 
+// Task 5
+
+const array = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+    ['@', '-', '_', ]
+];
+
+
+console.log(array[1][3]);
+console.log(Math.round(Math.random() * (2 - 0) + 1));
+console.log(Math.random() * (10 - 1) + 1);
+const outputPassword = document.querySelector('.output-password');
+const btnPassword = document.querySelector('.password-btn');
+let a;
+let b;
+
+const displayPassword = () => { 
+    const password = [];
+    for (let i = 0; i <= 8; i += 1) {
+        a = Math.round(Math.random() * (4 - 1));
+        if (a === 0) { 
+            b = Math.round(Math.random() * (9 - 1) + 1);
+        } else if (a === 1) { 
+            b = Math.round(Math.random() * (25 - 1) + 1);
+        } else if (a === 2) { 
+            b = Math.round(Math.random() * (25 - 1) + 1);
+        } else if (a === 3) { 
+            b = Math.round(Math.random() * (3 - 1));
+        } 
+        
+        console.log(`They are: ${a} and ${b}`);
+        password.push(array[a][b]);
+        outputPassword.textContent = password.join("");
+    }
+    console.log(password);
+}
+
+
+
+btnPassword.addEventListener("click", displayPassword);
