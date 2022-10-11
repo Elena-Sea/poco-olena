d19aba420c422af85217226fe5cad2304ddbf953
Elena-Sea <o.smetyuk@gmail.com>
2022-10-11 13:20:28 +0200

    Adds code for tasks 7 and 8

--- week3/js/calc.js
+++ week3/js/calc.js
@@ -33,15 +33,14 @@ const outputLength = document.querySelector('.display-length');
 
 
 const displayString = () => { 
-    outputString.textContent = inputString.value == "" ? "Nothing" : inputString.value;
-    outputLength.textContent = inputString.value.length;
+    outputString.textContent = inputString.value.trim() == "" ? "Nothing" : inputString.value.trim();
+    outputLength.textContent = inputString.value.trim().length;
 }
 
 inputString.addEventListener("input", displayString);
 // displayString();
 
 //  outputLength.textContent = outputString.textContent.length;
-
 // outputLength.textContent = inputString.value.length;
 
 // Task 3
@@ -94,11 +93,10 @@ const array = [
     ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
     ['@', '-', '_', ]
 ];
-
-
-console.log(array[1][3]);
-console.log(Math.round(Math.random() * (2 - 0) + 1));
-console.log(Math.random() * (10 - 1) + 1);
+// console.log(array[1][3]);
+// console.log(Math.round(Math.random() * (2 - 0) + 1));
+// console.log(Math.random() * (10 - 1) + 1);
+const inputPassword = document.querySelector('.input-password');
 const outputPassword = document.querySelector('.output-password');
 const btnPassword = document.querySelector('.password-btn');
 let a;
@@ -106,25 +104,91 @@ let b;
 
 const displayPassword = () => { 
     const password = [];
-    for (let i = 0; i <= 8; i += 1) {
+    for (let i = 0; i < inputPassword.value; i += 1) {
         a = Math.round(Math.random() * (4 - 1));
-        if (a === 0) { 
-            b = Math.round(Math.random() * (9 - 1) + 1);
-        } else if (a === 1) { 
-            b = Math.round(Math.random() * (25 - 1) + 1);
-        } else if (a === 2) { 
-            b = Math.round(Math.random() * (25 - 1) + 1);
-        } else if (a === 3) { 
-            b = Math.round(Math.random() * (3 - 1));
+        // if (a === 0) { 
+        //     b = Math.round(Math.random() * (9 - 1) + 1);
+        // } else if (a === 1) { 
+        //     b = Math.round(Math.random() * (25 - 1) + 1);
+        // } else if (a === 2) { 
+        //     b = Math.round(Math.random() * (25 - 1) + 1);
+        // } else if (a === 3) { 
+        //     b = Math.round(Math.random() * (3 - 1));
+        // } 
+
+        switch (a) { 
+            case 0:
+                b = Math.round(Math.random() * (9 - 1) + 1);
+                break;
+            case 1:
+                b = Math.round(Math.random() * (25 - 1) + 1);
+                break;
+            case 2:
+                b = Math.round(Math.random() * (25 - 1) + 1);
+                break;
+            case 3:
+                b = Math.round(Math.random() * (3 - 1));
+                break;
         } 
-        
-        console.log(`They are: ${a} and ${b}`);
+
+        // console.log(`They are: ${a} and ${b}`);
         password.push(array[a][b]);
+        // console.log(password);
         outputPassword.textContent = password.join("");
+        // console.log(password.join(""));
     }
-    console.log(password);
+    // console.log(password);
 }
 
-
-
 btnPassword.addEventListener("click", displayPassword);
+
+
+
+
+// Task 7
+const ref = {
+    inputYearBirth: document.querySelector('.input-year-birth'),
+    inputMonthBirth : document.querySelector('.input-month-birth'),
+    inputFutureYear : document.querySelector('.input-year-future'),
+    outputAge : document.querySelector('.display-age'),
+    outputYearFuture : document.querySelector('.display-year-future'),
+    btnAge : document.querySelector('.age-btn'),
+}
+
+const displayAge = () => { 
+    // if (ref.inputMonthBirth.value <= 6) {
+    //     ref.outputAge.textContent = ref.inputFutureYear.value - ref.inputYearBirth.value - 1;
+    // } else { 
+    //     ref.outputAge.textContent = ref.inputFutureYear.value - ref.inputYearBirth.value;
+    // }
+    ref.outputAge.textContent = ref.inputMonthBirth.value <= 6 ? ref.inputFutureYear.value - ref.inputYearBirth.value - 1 : ref.inputFutureYear.value - ref.inputYearBirth.value;
+    
+    ref.outputYearFuture.textContent = ref.inputFutureYear.value;
+}
+
+ref.btnAge.addEventListener("click", displayAge);
+
+
+
+
+// Task 8
+const refFortune = {
+    outputFortune : document.querySelector('.display-fortune'),
+    btnFortune : document.querySelector('.fortune-btn'),
+}
+
+const fortune = [
+    ["to Supermarket", "to Zoo", "home"],
+    ['eat a chocolate icecream', 'drink a cup of coffee', 'eat Pizza'],
+    ['Angelina Jolly','Your Granny','A big Mouse'],
+];
+
+
+
+const displayFortune = () => { 
+    const b = Math.round(Math.random() * (3 - 1));
+    const c = Math.round(Math.random() * (3 - 1));
+    const d = Math.round(Math.random() * (3 - 1));
+    refFortune.outputFortune.textContent = `Okey, so today afternoon you will go ${fortune[0][b].toUpperCase()}, there you ${fortune[1][c].toUpperCase()} and there you will meet ${fortune[2][d].toUpperCase()}!`;
+}
+refFortune.btnFortune.addEventListener("click", displayFortune);
