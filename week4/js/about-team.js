b1e87a3eacdc76a4f8db5ec0c7223c5578d265ee
Elena-Sea <o.smetyuk@gmail.com>
2022-10-14 12:43:18 +0200

    Adds team project before js effects

---
+++ week4/js/about-team.js
@@ -0,0 +1,273 @@
+'use strict';
+
+const minions = [
+  {
+    name: 'Abdul Fatah Shieryar',
+    image: './images/Abdula-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'AbdulFatah.Shieryar@powercoders.org',
+  },
+  {
+    name: 'Ali Göcmen',
+    image: './images/ali-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Ali.Gocmen@powercoders.org',
+  },
+{
+    name: 'Ni Luh Ariani',
+    image: './images/Ari-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Ariani.NiLuh@powercoders.org',
+  },
+{
+    name: 'Arwa Alhussein',
+    image: './images/Arwa-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Arwa.Alhussein@powercoders.org',
+  },
+{
+    name: 'Baris Durucan',
+    image: './images/Barish-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Baris.Durucan@powercoders.org',
+  },
+{
+    name: 'Bilal Sulaiman',
+    image: './images/bilal-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Bilal.Sulaimankhil@powercoders.org',
+  },
+{
+    name: 'Christina Graeni',
+    image: './images/christina-opt.jpg',
+	role: 'Senior minion',
+	function: 'Company Relations Romandie & PR',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'christina.graeni@powercoders.org',
+  },
+{
+    name: 'Emin Samed Yilmaz',
+    image: './images/Emin-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Emin.Yilmaz@powercoders.org',
+  },
+{
+    name: 'Enes Bora',
+    image: './images/Enes-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Enes.Bora@powercoders.org',
+	},
+  {
+    name: 'Fatih Yavuz',
+    image: './images/Fatih-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Fatih.Yavuz@powercoders.org',
+	},
+  {
+    name: 'Hasan Danaci',
+    image: './images/Hasan-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Hasan.Danaci@powercoders.org',
+	},
+  {
+    name: 'Hélène Portier',
+    image: './images/Helen-opt.jpeg',
+	role: 'Senior minion',
+	function: 'Training & Talent Development',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'helene.portier@powercoders.org',
+	},
+  {
+    name: 'İsmet Bakay',
+    image: './images/Ismet-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Ismet.Bakay@powercoders.org',
+	},
+  {
+    name: 'Jyothi Jogin',
+    image: './images/Joythi-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Jyothi.Jogin@powercoders.org',
+	},
+  {
+    name: 'K.Tuba Turkcan Karaca',
+    image: './images/Tuba-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Tuba.TurkcanKaraca@powercoders.org',
+	},
+  {
+    name: 'Linus Murbach',
+    image: './images/Linus-opt.jpeg',
+	role: 'Senior minion',
+	function: 'Program Manager',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'linus.murbach@powercoders.org',
+	},
+  {
+    name: 'Murat Güldibi',
+    image: './images/Murat-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Murat.Guldibi@powercoders.org',
+	},
+  {
+    name: 'Oguz Kaan Yildiz',
+    image: './images/Oguz-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Oguz.Yildiz@powercoders.org',
+	},
+  {
+    name: 'Olena Smetiuk',
+    image: './images/Olena-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Olena.Smetiuk@powercoders.org',
+	},
+  {
+    name: 'Sadettin Karatas',
+    image: './images/Sadethin-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Sadettin.Karatas@powercoders.org',
+	},
+  {
+    name: 'Sébastien de Graffenried',
+    image: './images/Sebastian-opt.jpeg',
+	role: 'Senior minion',
+	function: 'Teaching assistant',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'seb.degraffenried@powercoders.org',
+	},
+  {
+    name: 'Senay Atli',
+    image: './images/senay-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Senay.Atli@powercoders.org',
+	},
+  {
+    name: 'Serkan Tasgin',
+    image: './images/Serkan-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Serkan.Tasgin@powercoders.org',
+	},
+  {
+    name: 'Sevim Gokturk',
+    image: './images/Sevim-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Sevim.Gokturk@powercoders.org',
+	},
+  {
+    name: 'Shamil Idrisov',
+    image: './images/Shamil-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Shamil.Idrisov@powercoders.org',
+	},
+  {
+    name: 'Yuliia Kuts',
+    image: './images/Yulia-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Yuliia.Kuts@powercoders.org',
+	},
+  {
+    name: 'Yusuf Ilhan',
+    image: './images/Yusuf-opt.jpeg',
+	role: 'Talented minion',
+	function: 'Training, training, training...',
+	linkedin: 'https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo',
+	mail: 'Yusuf.Ilhan@powercoders.org',
+  },
+];
+
+// , , 
+
+// , , , , , , , , , , , , , , , , , , , stacy.voncranach@powercoders.org, tarek.kariish@powercoders.org, powercoders@tristanlabelle.com, yaara.segal@powercoders.org, , 
+
+const ref = {
+  teamGallery: document.querySelector('.team-gallery'),
+   
+}
+
+function markupTeamGallery(minions) { 
+    ref.teamGallery.insertAdjacentHTML('beforeend', minions.map(minion => { 
+        return `<li class="minion-card">
+                    <div class="minion-card__thumb">
+                        <img class="minion-card__image" src="${minion.image}" width="200" alt="${minion.name} photo"/>
+                        <div class="minion">
+                            <h3 class="minion__name">${minion.name.toUpperCase()}</h3>
+                            <p class="minion__role">${minion.role}</p>
+							<p class="function">${minion.function}</p>
+                            <ul class="minion__links">
+                                <li class="minion__linkedin">
+                                    <a href="#" class="minion__linkedin-link">
+                                    <svg class="minion__linkedin-icon" width="32" height="32">
+                                        <use href="#icon-linkedin"></use>
+                                    </svg>
+                                    </a>
+                                </li>
+                                <li class="minion__mail">
+                                    <a href="${minion.mail}" class="minion__mail-icon">
+                                    <svg class="minion__mail-icon" width="32" height="32">
+                                        <use href="#icon-mail"></use>
+                                    </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>`
+    }).join('')
+    );
+}
+
+markupTeamGallery(minions);
+// const displayAge = () => { 
+   
+//     ref.outputAge.textContent = ref.inputMonthBirth.value <= 6 ? ref.inputFutureYear.value - ref.inputYearBirth.value - 1 : ref.inputFutureYear.value - ref.inputYearBirth.value;
+    
+//     ref.outputYearFuture.textContent = ref.inputFutureYear.value;
+// }
+
+// ref.btnAge.addEventListener("click", displayAge);
+
+
